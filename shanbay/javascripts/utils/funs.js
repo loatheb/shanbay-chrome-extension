d4a263d4956459363a3444efc92d9ef8a870fd72
zz-zhangzhao <zz.innj@gmail.com>
2017-01-28 19:44:58 +0800

    add shanbay.crx in ignore file; change utils function;

--- shanbay/javascripts/utils/funs.js
+++ shanbay/javascripts/utils/funs.js
@@ -4,4 +4,4 @@ var qsa = document.querySelectorAll.bind(document);
 
 var qs = document.querySelector.bind(document);
 
-var getArray = Function.prototype.bind.call(Array.prototype.slice);
+var getArray = Function.prototype.apply.bind(Array.prototype.slice);
